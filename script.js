7163e7b8896c5c049b1fb34a55d4989f2448d915
Dipankar-koley <koleydipankar21@gmail.com>
2023-12-23 12:56:09 +0530

    changes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var typed = new Typed("")
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('#multiple-text', {
+        strings: ["Frontend Developer","Web Backend Developer","YouTuber"],
+      typeSpeed: 70,
+      backSpeed: 30,
+    backDelay: 800,
+    loop: true
+    });
+  });
